5ca62e00844ca2a02f85f7da145dee0e2d7e6504
ostriandoni <donny.ostrianto@gmail.com>
2021-12-10 13:10:38 +0700

    finishing boss level challenge 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blueAccent.shade400,
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ball = 1;
+
+  void randomNumber() {
+    setState(() {
+      ball = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                randomNumber();
+              },
+              child: Image.asset('images/ball$ball.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
